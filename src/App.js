096b95dd982bc2f83a9793db128deb6253af4914
Zuhair <zuhairhassan52@gmail.com>
2024-10-04 22:59:12 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#menu">menu</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <sectio>
+          <h1>Welcome to little lemon resturant</h1>
+          <p>Reserve a table and enjoy your meal</p>
+        </sectio>
+      </main>
+      <footer>
+        <p>2024 little limon</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
